9bde11c0feb4ada5dcb91655a0bbf9aef2caa020
Bulat <bulat067@gmail.com>
2026-08-21 03:29:32 +0300

    Remove header actions

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Logo from '../img/logo.svg';
 import { FavoriteButton } from './FavoriteButton';
 import { HeaderTabs } from './HeaderTabs';
-import { HeaderActions } from './HeaderActions';
 import { UserPanel } from './UserPanel';
 import styles from './Header.module.scss';
 
@@ -17,8 +16,6 @@ export const Header = () => {
           <HeaderTabs />
         </nav>
       </div>
-
-      <HeaderActions />
 
       <UserPanel />
     </header>
